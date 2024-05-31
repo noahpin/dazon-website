a7ffb831ff0ddce5d1d4d65f00f043d3ef92ce05
Noah Pinales <pinales@usc.edu>
2024-05-30 21:29:11 -0700

    huh

---
+++ src/lib/components/AsthaLogo.svelte
@@ -0,0 +1,52 @@
+
+<script>
+    export let width = 100;
+</script>
+<svg
+	id="uuid-e9fbd5e2-9826-4727-8117-2f285cd4d08c"
+	data-name="Layer 2"
+	xmlns="http://www.w3.org/2000/svg"
+	{width}
+	viewBox="0 0 69.052 77.128"
+>
+	<g id="uuid-06179082-7991-44b1-9253-2644dbfafb1d" data-name="novelties">
+		<g>
+			<circle cx="34.526" cy="8.413" r="8.413" fill="#fff" stroke-width="0" />
+			<circle
+				cx="60.637"
+				cy="23.487"
+				r="8.413"
+				fill="#f7f3a5"
+				stroke-width="0"
+			/>
+			<circle
+				cx="60.639"
+				cy="53.639"
+				r="8.413"
+				fill="#a5dad0"
+				stroke-width="0"
+			/>
+			<circle
+				cx="34.526"
+				cy="68.715"
+				r="8.413"
+				fill="#77d0e9"
+				stroke-width="0"
+			/>
+			<circle
+				cx="8.413"
+				cy="53.639"
+				r="8.413"
+				fill="#bb9cc9"
+				stroke-width="0"
+			/>
+			<circle
+				cx="8.415"
+				cy="23.487"
+				r="8.413"
+				fill="#ebb2d1"
+				stroke-width="0"
+			/>
+		</g>
+	</g>
+</svg>
